688d7875ea94f38cb73521a331bc0ead621e6126
Salah Alayoubi <Salahalayoubi23@gmail.com>
2023-03-11 15:17:05 -0500

    FIXED IT :smiley:

--- astro_weather/lib/login_register/login.dart
+++ astro_weather/lib/login_register/login.dart
@@ -3,33 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 
-class LoginPage extends State<LoginPageState> with TickerProviderStateMixin{
+
+class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> with TickerProviderStateMixin{
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedBackground(
-      behaviour: RandomParticleBehaviour(
-        options: const ParticleOptions(
-          spawnMaxRadius: 50,
-          spawnMinSpeed: 10.00,
-          particleCount: 68,
-          spawnMaxSpeed: 50,
-          minOpacity: 0.3,
-          spawnOpacity: 0.4,
-          baseColor: Colors.black,
+      body: Container(
+        child: AnimatedBackground(
+        behaviour: RandomParticleBehaviour(
+          options: const ParticleOptions(
+            spawnMaxRadius: 50,
+            spawnMinSpeed: 10.00,
+            particleCount: 68,
+            spawnMaxSpeed: 50,
+            minOpacity: 0.3,
+            spawnOpacity: 0.4,
+            baseColor: Colors.black,
+          ),
         ),
+        vsync: this,
+        child: Text('Hello')
+          ),
       ),
-      vsync: this,
-      child: Text('Hello')
-    ),
     );
   }
 }
 
-class LoginPageState extends StatefulWidget{
-  const LoginPageState({super.key});
-  @override
-  State<LoginPageState> createState() => LoginPage();
-}
 
