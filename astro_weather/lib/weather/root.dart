953cad8b696d950943b81c9ee1c5f5f14ac01782
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2023-03-14 11:50:52 -0400

    root page start
    animated sun/moon, base layout

---
+++ astro_weather/lib/weather/root.dart
@@ -0,0 +1,96 @@
+import 'package:astro_weather/weather/info.dart';
+import 'package:astro_weather/weather/stars.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+import 'package:flutter/services.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  // **get time of day**
+  double timeOfDay = 0.0;
+  bool isNight = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollUpdateNotification>(
+      onNotification: (notification) {
+        final scrollDelta = notification.scrollDelta;
+        if (scrollDelta != null && scrollDelta < 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => StarPage()),
+          );
+        } else if (scrollDelta != null && scrollDelta > 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => InfoPage()),
+          );
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  AnimatedPositioned(
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.easeInOut,
+                    left: (MediaQuery.of(context).size.width - 100) * timeOfDay,
+                    child: Image.asset(
+                      (isNight == true)
+                          ? 'assets/weather/moon.png'
+                          : 'assets/weather/sun.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Slider(
+              value: timeOfDay,
+              onChanged: (value) {
+                setState(() {
+                  timeOfDay = value;
+                  if (timeOfDay == 1.0 && isNight == false) {
+                    timeOfDay = 0.0;
+                    isNight = true;
+                  } else if (timeOfDay == 1.0 && isNight == true) {
+                    timeOfDay = 0.0;
+                    isNight = false;
+                  }
+                });
+              },
+              min: 0.0,
+              max: 1.0,
+              divisions: 1200,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+              child: Container(
+                color: Colors.purple,
+                height: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
