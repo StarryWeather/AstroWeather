cc83e7090f4b754a67cb0ed5359cf84792890159
Zimbee <61887028+Zmbeboy@users.noreply.github.com>
2023-03-14 13:58:03 -0400

    I made a bunch of buttons to learn flutter better
    its neat

--- astro_weather/lib/login_register/login.dart
+++ astro_weather/lib/login_register/login.dart
@@ -1,22 +1,89 @@
+import 'package:astro_weather/Eric.dart';
 import 'package:flutter/material.dart';
 import 'background.dart';
 import 'package:page_transition/page_transition.dart';
 
-
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
   @override
   State<LoginPage> createState() => LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>{
-  
+class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SetBackground(),
+      body: Container(
+        color: Colors.amber.shade500,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  //print("ButtonClicked");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SetBackground()));
+                },
+                child: Container(
+                  height: 50,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.purple,
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text("To the Earth!"),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const EricTest()));
+                },
+                child: Container(
+                  height: 50,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.purple,
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      "To Eric's Test Area",
+                      style: TextStyle(
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
-
