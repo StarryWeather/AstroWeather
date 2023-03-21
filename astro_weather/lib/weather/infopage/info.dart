c3617bf107dd0430f5710346c344a88d8103ad22
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2023-03-21 17:10:29 -0400

    init info page
    init info page start and gesture detection to navigate pages

--- astro_weather/lib/weather/infopage/info.dart
+++ astro_weather/lib/weather/infopage/info.dart
@@ -10,6 +10,62 @@ class InfoPage extends StatefulWidget {
 class _InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
-    return Text('InfoPage');
+    return Scaffold(
+      body: GestureDetector(
+        onVerticalDragUpdate: (details) {
+          if (details.delta.dy < 0) {
+            Navigator.pop(context);
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              height: MediaQuery.of(context).size.height * 0.25,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+
+            // Orange box in the second row
+            Container(
+              width: MediaQuery.of(context).size.width * 0.75,
+              height: MediaQuery.of(context).size.height * 0.20,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.35,
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                SizedBox(width: MediaQuery.of(context).size.width * 0.07),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.35,
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- astro_weather/lib/weather/rootpage/root.dart
+++ astro_weather/lib/weather/rootpage/root.dart
@@ -1,13 +1,9 @@
-import 'dart:ui';
 import 'package:astro_weather/weather/infopage/info.dart';
 import 'package:astro_weather/weather/rootpage/land.dart';
 import 'package:astro_weather/weather/rootpage/stars.dart';
 import 'package:astro_weather/weather/rootpage/sunMoon.dart';
 import 'package:astro_weather/weather/starspage/stars.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter/services.dart';
-import 'package:proste_bezier_curve/proste_bezier_curve.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -24,21 +20,19 @@ class _RootPageState extends State<RootPage> {
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollUpdateNotification>(
-      onNotification: (notification) {
-        final scrollDelta = notification.scrollDelta;
-        if (scrollDelta != null && scrollDelta < 0) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => StarPage()),
-          );
-        } else if (scrollDelta != null && scrollDelta > 0) {
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        if (details.delta.dy < 0) {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => InfoPage()),
           );
+        } else if (details.delta.dy > 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => StarPage()),
+          );
         }
-        return true;
       },
       child: Scaffold(
         body: Column(
