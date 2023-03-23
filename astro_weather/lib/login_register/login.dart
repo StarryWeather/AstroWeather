384a63c0d5a6d315c44bf75fea56bb1135bae62a
Zimbee <61887028+Zmbeboy@users.noreply.github.com>
2023-03-23 16:52:55 -0400

    I did it!

--- astro_weather/lib/login_register/login.dart
+++ astro_weather/lib/login_register/login.dart
@@ -1,11 +1,11 @@
 import 'package:astro_weather/login_register/register.dart';
 import 'package:flutter/material.dart';
+import 'package:starsview/starsview.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../weather/rootpage/root.dart';
+import 'earthsetter.dart';
 import 'package:page_transition/page_transition.dart';
-
-import 'package:astro_weather/Eric.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -21,142 +21,171 @@ class LoginPageState extends State<LoginPage> {
     final emailController = TextEditingController();
     final passwordController = TextEditingController();
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            //welcome back,
-            const Text(
-              'Welcome to StarryWeather',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 16),
-
-            // email textfield
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                // set error style if email is invalid
-                errorText: isEmailValid ? null : 'Invalid Email',
-                errorBorder: isEmailValid
-                    ? null
-                    : OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.red),
-                      ),
-              ),
-            ),
-            const SizedBox(height: 16),
-
-            //password textfield
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                // set error style if password is invalid
-                errorText: isPasswordValid ? null : 'Invalid Password',
-                errorBorder: isPasswordValid
-                    ? null
-                    : OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.red),
-                      ),
-              ),
-            ),
-            const SizedBox(height: 16),
-
-            //forgot password?
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    // FORGOT PASSWORD PAGE
-                  },
-                  child: Text('Forgot Password'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            //sign in button
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      // API LOGIN CALL
-                      var url = Uri.parse(
-                          'https://hidden-tor-21438.herokuapp.com/api/users/login');
-                      var data = {
-                        'email': emailController.text,
-                        'password': passwordController.text
-                      };
-                      var jsonData = jsonEncode(data);
-                      var response = await http.post(url,
-                          headers: {"Content-Type": "application/json"},
-                          body: jsonData);
-                      // check if valid
-                      if (response.statusCode == 200) {
-                        // true: go to root
-                        debugPrint('Made it in here: pass');
-                        var responseJSON = json.decode(response.body);
-                        //var responseJSON = json.decode(response.body);
-                        Navigator.push(
-                          context,
-                          PageTransition(
-                            type: PageTransitionType.leftToRightPop,
-                            child: RootPage(
-                              accessToken: responseJSON['accessToken'],
-                            ),
-                            duration: Duration(milliseconds: 400),
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      body: Stack(
+        children: [
+          const StarsView(
+            fps: 60,
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(32.0),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Color.fromARGB(160, 95, 95, 95),
+                    border: Border.all(width: 5.0, color: Colors.white),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(32.0),
+                    child: Column(
+                      children: [
+                        //welcome back,
+                        const Text(
+                          'Welcome to AstroWeather',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontFamily: 'KdamThmorPro',
                           ),
-                        );
-                      } else {
-                        // false: display email/password invalid
-                        debugPrint('Made it in here: fail');
-                        debugPrint(emailController.text);
-                        debugPrint(passwordController.text);
-                        isEmailValid = false;
-                        isPasswordValid = false;
-                      }
-                    },
-                    child: const Text('Login'),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        PageTransition(
-                          type: PageTransitionType.bottomToTop,
-                          child: RegisterPage(),
-                          duration: Duration(milliseconds: 400),
                         ),
-                      );
-                    },
-                    child: const Text('Register'),
+
+                        const SizedBox(height: 16),
+
+                        // email textfield
+                        TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            // set error style if email is invalid
+                            errorText: isEmailValid ? null : 'Invalid Email',
+                            errorBorder: isEmailValid
+                                ? null
+                                : OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(color: Colors.red),
+                                  ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+
+                        //password textfield
+                        TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            // set error style if password is invalid
+                            errorText:
+                                isPasswordValid ? null : 'Invalid Password',
+                            errorBorder: isPasswordValid
+                                ? null
+                                : OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(color: Colors.red),
+                                  ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+
+                        //forgot password?
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                // FORGOT PASSWORD PAGE
+                              },
+                              child: Text('Forgot Password'),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 16),
+                        //sign in button
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () async {
+                                  // API LOGIN CALL
+                                  var url = Uri.parse(
+                                      'https://hidden-tor-21438.herokuapp.com/api/users/login');
+                                  var data = {
+                                    'email': emailController.text,
+                                    'password': passwordController.text
+                                  };
+                                  var jsonData = jsonEncode(data);
+                                  var response = await http.post(url,
+                                      headers: {
+                                        "Content-Type": "application/json"
+                                      },
+                                      body: jsonData);
+                                  // check if valid
+                                  if (response.statusCode == 200) {
+                                    // true: go to root
+                                    debugPrint('Made it in here: pass');
+                                    var responseJSON =
+                                        json.decode(response.body);
+                                    //var responseJSON = json.decode(response.body);
+                                    Navigator.push(
+                                      context,
+                                      PageTransition(
+                                        type: PageTransitionType.leftToRightPop,
+                                        child: RootPage(
+                                          accessToken:
+                                              responseJSON['accessToken'],
+                                        ),
+                                        duration: Duration(milliseconds: 400),
+                                      ),
+                                    );
+                                  } else {
+                                    // false: display email/password invalid
+                                    debugPrint('Made it in here: fail');
+                                    isEmailValid = false;
+                                    isPasswordValid = false;
+                                  }
+                                },
+                                child: const Text('Login'),
+                              ),
+                            ),
+                            const SizedBox(width: 16),
+                            Expanded(
+                              child: OutlinedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    PageTransition(
+                                      type: PageTransitionType.bottomToTop,
+                                      child: RegisterPage(),
+                                      duration: Duration(milliseconds: 400),
+                                    ),
+                                  );
+                                },
+                                child: const Text('Register'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        //sign in with google
+
+                        //register
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
-            //sign in with google
-
-            //register
-          ]),
-        ),
+              ),
+              earthState(),
+            ],
+          ),
+        ],
       ),
     );
   }
