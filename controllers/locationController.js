598aa457cefb94d31d70a7af9089f6fdd9f36561
Daniel Cruz-Castro <60457928+DanDigits@users.noreply.github.com>
2023-04-30 18:13:22 -0400

    Testing location addition

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -55,7 +55,7 @@ const createLocation = asyncHandler(async (req, res) => {
         console.log("Location exists");
         const coordinates = {latitude: lat, longitude: long};
         const updatedLocation = await Location.findByIdAndUpdate(id, {$push: {savedLocations: coordinates}});
-        if (!updateLocation) {
+        if (!updatedLocation) {
             res.status(404);
             throw new Error("Error updating location");
         } else {
