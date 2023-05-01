c5d66a8a8e1b81e8c06746a4f101eb3359610b0c
Eric Gass <ericjohngass@gmail.com>
2023-05-01 19:44:16 -0400

    BAD BAD BAD

--- astro_weather/lib/utils/AstroAPI.dart
+++ astro_weather/lib/utils/AstroAPI.dart
@@ -7,7 +7,10 @@ import 'package:http/http.dart' as http;
 
 
 Future<void> jellyfish(lat, long) async {
-  var authToken = window.btoa("54c0e856-effd-4386-9fa8-e30936d4569b:8e37e3b5728d880694f78e6168c03fe5f4a570da15eaf9b10baf18670bfad673f329eee1c04425bcbe5618bcbe9c80c17bf0904bca00c8480847d2e45cf0386439f278a1d048ecea61630268a3b047967c7d852c06e67773ea9ae729e1125f5da71fb9d2c64c84b4c508cd772112c1e9");
+  var code = ("54c0e856-effd-4386-9fa8-e30936d4569b:8e37e3b5728d880694f78e6168c03fe5f4a570da15eaf9b10baf18670bfad673f329eee1c04425bcbe5618bcbe9c80c17bf0904bca00c8480847d2e45cf0386439f278a1d048ecea61630268a3b047967c7d852c06e67773ea9ae729e1125f5da71fb9d2c64c84b4c508cd772112c1e9");
+  Codec<String, String> stringToBase64 = utf8.fuse(base64);
+  String authToken = stringToBase64.encode(code);
+  
   var url = Uri.parse('https://api.astronomyapi.com/api/v2/studio/star-chart');
   var day = DateTime.now().day;
   var day2;
