49f7cfb4d574905548c72dab4d165aaed1448f26
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2023-05-01 19:04:39 -0400

    insane displaying

--- astro_weather/lib/screens/rootpage/root.dart
+++ astro_weather/lib/screens/rootpage/root.dart
@@ -50,7 +50,7 @@ class _RootPageState extends State<RootPage> {
             context,
             MaterialPageRoute(builder: (context) => InfoPage()),
           );
-        }else if (details.delta.dy > 0) {
+        } else if (details.delta.dy > 0) {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => StarView()),
@@ -59,79 +59,131 @@ class _RootPageState extends State<RootPage> {
       },
       child: Scaffold(
         body: SizedBox.expand(
-            child: Container(
-                decoration: BoxDecoration(gradient: skyColor),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(gradient: skyColor),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
                   children: [
-                    Stack(
-                      children: [
-                        Container(
-                          //this might need to be a stack find out later
-                          child: SunMoon(),
-                          height: MediaQuery.of(context).size.height,
-                        ),
-                        if (cloudIndex == 0) Container(),
-                        if (cloudIndex == 1)
-                          Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage('assets/weather/50.png'),
-                              ),
-                            ),
-                            height: MediaQuery.of(context).size.height / 2,
+                    Container(
+                      //this might need to be a stack find out later
+                      child: SunMoon(),
+                      height: MediaQuery.of(context).size.height,
+                    ),
+                    if (cloudIndex == 0) Container(),
+                    if (cloudIndex == 1)
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/weather/50.png'),
                           ),
-                        if (cloudIndex == 2)
-                          Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage('assets/weather/darkSky.png'),
-                              ),
-                            ),
-                            height: MediaQuery.of(context).size.height / 2,
-                          ),
-                          GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => StarPage()));
-                          },
-                          child: Stars(),
                         ),
-                        Positioned.fill(
-                          child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => InfoPage()));
-                              },
-                              child: Container(
-                                height: MediaQuery.of(context).size.height*0.55,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: (DN == "d")
-                                        ? AssetImage(
-                                            'assets/weather/xpgrass.png')
-                                        : AssetImage(
-                                            'assets/weather/nightxpgrass.png'),
-                                    opacity: 1,
-                                    fit: BoxFit.fill,
+                        height: MediaQuery.of(context).size.height / 2,
+                      ),
+                    if (cloudIndex == 2)
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/weather/darkSky.png'),
+                          ),
+                        ),
+                        height: MediaQuery.of(context).size.height / 2,
+                      ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => StarPage()));
+                      },
+                      child: Stars(),
+                    ),
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(15, 255, 255, 255),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: Container(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        globals.currentTemp + "\u00B0",
+                                        style: TextStyle(
+                                          fontSize: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.02,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontFamily: 'KdamThmorPro',
+                                        ),
+                                      ),
+                                      Text(
+                                        globals.weatherDescription,
+                                        style: TextStyle(
+                                          fontSize: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.02,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontFamily: 'KdamThmorPro',
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
                             ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => InfoPage()));
+                          },
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.55,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: (DN == "d")
+                                    ? AssetImage('assets/weather/xpgrass.png')
+                                    : AssetImage(
+                                        'assets/weather/nightxpgrass.png'),
+                                opacity: 1,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
                           ),
                         ),
-                      ],
+                      ),
                     ),
                   ],
-                ))),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
